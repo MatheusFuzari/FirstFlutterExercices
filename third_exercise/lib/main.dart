8eb452357b31a23939099c91f39f37ef11fd5072
Desenvolvimento <dsadm@educ501.local>
2024-02-16 22:48:55 -0300

    revamp

--- third_exercise/lib/main.dart
+++ third_exercise/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,28 +28,97 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> _drivers = ["Lewis Hamilton", "Sebastian Vettel", "Max Verstappen", "Charles Leclerc"];
+  final List<String> _drivers = [
+    "Lewis Hamilton",
+    "Sebastian Vettel",
+    "Max Verstappen",
+    "Charles Leclerc"
+  ];
   final List<String> _driversAge = ["39", "36", "26", "26"];
   final List<String> _driversChampionship = ["7", "4", "3", "0"];
-  final List<String> _driversImage = ["assets/lewis","assets/vettel","assets/max","assets/leclerc"];
+  final List<String> _driversImage = [
+    "assets/lewis",
+    "assets/vettel",
+    "assets/max",
+    "assets/leclerc"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Drivers"),
-      ),
-      body: Center(
-        child: ListView.separated(
-          padding: EdgeInsets.all(8.0),
-          itemCount: _drivers.length,
-          itemBuilder: (BuildContext context, index){
-            return Container();
-          },
-          separatorBuilder: (BuildContext context, index) => const Divider(),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Drivers?"),
         ),
-      )
-    );
+        body: Center(
+          child: ListView.separated(
+            padding: const EdgeInsets.all(8.0),
+            itemCount: _drivers.length,
+            itemBuilder: (BuildContext context, index) {
+              return Container(
+                height: 160,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Colors.black,
+                ),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 160,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('${_driversImage[index]}.png'),
+                            fit: BoxFit.fill,
+                          ),
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text(
+                            _drivers[index],
+                            style: TextStyle(
+                                color: Colors.red.shade800,
+                                fontSize: 15,
+                                fontFamily: "Persona",
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Icon(
+                                  FontAwesomeIcons.crown,
+                                  size: 12,
+                                  color: Colors.red.shade800,
+                                ),
+                              ),
+                              Text(
+                                "   =  ${_driversChampionship[index]}x",
+                                style: TextStyle(
+                                    color: Colors.red.shade800,
+                                    fontSize: 15,
+                                    fontFamily: "Persona",
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, index) => const Divider(),
+          ),
+        ));
   }
 }
